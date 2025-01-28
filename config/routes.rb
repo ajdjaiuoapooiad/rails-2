b3c3f7878d4b0953f33a05bd7b77ed81a0edc6ba
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-28 22:40:02 +0900

    new ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root "post#index"
+
+  get 'user' => 'user#index'
+  get 'user/new' => 'user#new'
+  # New先に書く
+  get 'user/:id' => 'user#detail'
+  post 'user/new' => 'user#create'
+  get 'user/:id/edit' => 'user#edit'
+  post 'user/:id/edit' => 'user#update'
 end
