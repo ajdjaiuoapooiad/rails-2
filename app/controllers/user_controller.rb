12aec3ba755be4d43d5b4dbaf87ea5b6760aac74
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-29 00:01:01 +0900

    error

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,20 @@
 class UserController < ApplicationController
+  def index 
+    
+  end
+
+  def new 
+    @post = User.new
+  end
+
+  def create 
+    @post = User.new(post_params)
+    @post.save 
+    redirect_to users_path
+  end
+
+  private 
+  def post_params
+    params.require(:user).permit(:name, :email) #パラメーターのキー
+  end
 end
