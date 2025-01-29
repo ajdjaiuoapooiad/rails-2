0338f3ea761522017522d16f893fdb25185d0c26
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-29 10:09:41 +0900

    ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,15 +12,35 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(post_params)
     if @user.save 
-      redirect_to users_path
+      redirect_to action: :index
     else
       render :new
     end
   end
 
-  def detail 
+  def show 
     @user = User.find(params[:id])
   end
+
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def edit 
+    @user = User.find(params[:id])
+    if @user.update(post_params)
+      redirect_to users_path
+    else
+      render :edit 
+    end
+  end
+
+  def destroy 
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
 
 
   private 
