c9e8e25b1657e43669aad93d5363ee95fb8334f2
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-29 09:39:41 +0900

    errro

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,20 +1,34 @@
 class UserController < ApplicationController
+  before_action :set_post, only: [:edit, :update]
+
   def index 
-    
+    @users = User.all
   end
 
   def new 
-    @post = User.new
+    @user = User.new
   end
 
   def create 
-    @post = User.new(post_params)
-    @post.save 
-    redirect_to users_path
+    @user = User.new(post_params)
+    if @user.save 
+      redirect_to users_path
+    else
+      render :new
+    end
   end
+
+  def detail 
+    @user = User.find(params[:id])
+  end
+
 
   private 
   def post_params
     params.require(:user).permit(:name, :email) #パラメーターのキー
+  end
+
+  def set_post
+    @user = User.find(params[:id])
   end
 end
